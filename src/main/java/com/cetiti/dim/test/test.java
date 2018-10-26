1d4cd9e4691c8bc4800e768509849a6452d33a83
sofo2016 <chenshijie@cetiti.com>
2018-10-26 17:26:01 +0800

    get some message

---
+++ src/main/java/com/cetiti/dim/test/test.java
@@ -0,0 +1,15 @@
+/*
+ * File Name: test.java
+ * Copyright: (C) 2017 CETITI
+ * Description: 
+ * Author: chensj
+ * Create Date: 2018-10-26 17:25
+ * Modifier: lenovo
+ * Modify Date: 2018-10-26
+ * Bugzilla Id: 
+ * Modify Content: 
+ */
+package com.cetiti.dim.test;
+
+public class test {
+}
